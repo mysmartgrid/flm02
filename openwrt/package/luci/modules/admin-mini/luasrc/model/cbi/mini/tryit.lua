0eb81983913e5b2681d07d85588e70a22554f947
paalsteek <stephan.platz@googlemail.com>
2011-08-19 11:47:33 +0200

    proted tryit to new flukso api

--- openwrt/package/luci/modules/admin-mini/luasrc/model/cbi/mini/tryit.lua
+++ openwrt/package/luci/modules/admin-mini/luasrc/model/cbi/mini/tryit.lua
@@ -13,81 +13,43 @@ You may obtain a copy of the License at
 $Id: network.lua 4171 2009-01-27 20:50:28Z Cyrus $
 ]]--
 
-f = SimpleForm("tryit", "Test Connection", "Trying to connect to the server of the MySmartgrid project")
+f = SimpleForm("tryit", "Test Connection", "Trying to connect to the MySmartgrid server")
 
-uci = luci.model.uci.cursor()
-
--- Get wireless interface
-wifidevs={}
-uci:foreach("wireless", "wifi-device",
-	function(section)
-		table.insert(wifidevs, section[".name"])
-	end)
-
-if uci:get("network", "wan", "proto") == "none" then
-	uci:set("wireless", wifidevs[1], "disabled", 1) -- Disable wireless interface
-else
-	uci:delete("wireless", wifidevs[1], "disabled") -- Activate wireless interface
+function fsync() --run fsync and parse output
+	local p = io.popen("/usr/bin/fsync")
+	local result = {}
+	for line in p:lines() do
+		if line:sub(0,4) == "POST" then
+			result["code"] = line:sub(-4):gsub(" ", "")
+			message = p:read()
+			if message ~= nil then
+				result["message"] = message
+			end
+		end
+	end
+	p:close()
+	return result
 end
-uci:save("wireless")
-uci:commit("wireless")
-uci:save("network")
-uci:commit("network")
-uci:apply({"wireless", "network"}) -- Apply changes
-
-function heartbeat()
-  require 'posix'
-  require 'xmlrpc.http'
-  require 'luci.sys'
-
-  auth = require('flukso.auth')
-  dbg  = require('flukso.dbg')
-
-  -- config parameters
-  local param = {server        = 'dev2-logger.mysmartgrid.de',
-                 xmlrpcaddress = 'http://dev2-logger.mysmartgrid.de/xmlrpc',
-                 xmlrpcversion = '1',
-                 xmlrpcmethod  = 'logger.heartbeat'}
-
-  local monitor = {reset = tonumber(arg[1])}
-
-  -- open the connection to the syslog deamon, specifying our identity
-  posix.openlog('heartbeat')
-
-  -- calculate hmac and collect relevant monitoring points
-  local auth = auth.new()
-  auth:load()
-  monitor.version = tonumber(auth.version)
-
-  monitor.uptime  = math.floor(luci.sys.uptime())
-  monitor.uart_oe = string.match(luci.sys.exec('cat /proc/tty/driver/serial'), 'oe:(%d+)') or 0
-  system, model, monitor.memtotal, monitor.memcached, monitor.membuffers, monitor.memfree = luci.sys.sysinfo()
-
-  auth:hmac(monitor)
-
-  dbg.vardump(auth)
-  dbg.vardump(monitor)
-
-  -- send a heartbeat method call
-  local pcall_ok, return_or_err, pong = pcall(xmlrpc.http.call,
-    param.xmlrpcaddress..'/'..param.xmlrpcversion,
-    param.xmlrpcmethod,
-    auth,
-    monitor)
-
-	return pcall_ok, return_or_err, pong
-end
-
-res, err, pong = heartbeat()
 
 result = f:field(DummyValue, "", "")
 function result.cfgvalue(self, section)
-	if res then
-		return "Verbindungsaufbau erfolgreich"
-	elseif err:find("host not found") then
-		return "Konnte keine Verbindung zum mySmartGrid Server herstellen. "
+	local fsync = fsync()
+	local code = tonumber(fsync["code"])
+	if code == 200 then
+		return "Verbindung zum Server erfolgreich hergestellt."
+	--[[ 
+	elseif code > 200 then --check if the problem is the server or the connection
+		if fsync["message"] ~= nil then
+			return "Fehler (" .. fsync["message"] .. " [" .. fsync["code"] .. "])"
+		else
+			return "Fehler (" .. fsync["message"] .. ")"
+		end]]--
 	else
-		return "Fehler aufgetreten: " .. err
+		if fsync["message"] ~= nil then
+			return "Es konnte keine Verbindung zum Server hergestellt werden (" .. fsync["message"] .. " [" .. fsync["code"] .. "])"
+		else
+			return "Es konnte keine Verbindung zum Server hergestellt werden (" .. fsync["code"] .. ")"
+		end
 	end
 end
 
