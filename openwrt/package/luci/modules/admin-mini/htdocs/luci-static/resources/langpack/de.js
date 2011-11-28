297674d6eb8058877147aa8495eae4c885ada9e8
ely.oliveira <ely.oliveira@itwm.fraunhofer.de>
2011-11-28 11:58:44 +0100

    The dashboard bar is now hidden by default and accessible via link 'Expert Options'. The app is multi language, and flag icons allow the user to switch between German and English. The welcome page is currently the only page affected by the translation, and serves as an example for the other ones.

---
+++ openwrt/package/luci/modules/admin-mini/htdocs/luci-static/resources/langpack/de.js
@@ -0,0 +1,19 @@
+jquery_lang_js.prototype.lang.de = {
+	'Expert Options':'Experten-Optionen',
+	'Welcome':'Willkommen',
+	'Sensor':'Sensor',
+	'Wifi':'Wifi',
+	'Network':'Netzwerk',
+	'System':'System',
+	'Syslog':'Syslog',
+	'Registration':'Registrierung',
+	'Before you can use your Flukso, a few more steps are necessary. They must be taken in the order described below.':'Bevor sie ihren Flukso nutzen können sind noch ein paar Schritte notwendig. Diese müssen in der unten beschriebenen Reihenfolge durchgeführt werden.',
+        'To configure the network connection, please go to the':'Um die Netzwerkverbindung des Flukso zu konfigurieren gehen sie bitte zur',
+        'WiFi page':'WiFi-Seite',
+        'and inform your WLAN settings.':'und nehmen die notwendigen Einstellungen für ihr WLAN vor.',
+	'Then, please configure the Flukso sensors at the':'Anschlie..end konfigurieren sie bitte die Sensoren ihres Flukso auf der',
+	'sensor page':'Sensor-Seite',
+	'Finally, please follow the instructions on the':'Zum Schluss folgen sie bitte den Anweisungen auf der',
+	'registration page':'Registration-Seite',
+	'in order to link the Flukso device with your mySmartGrid account.':'um den Flukso mit ihrem mySmartGrid-Account zu verknüpfen.'
+}
